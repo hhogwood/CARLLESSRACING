44b94a0f5d891f46618303990969a2ab323c7e62
jhogwood <joseph.hogwood@gmail.com>
2014-03-23 14:55:45 -0500

    tempooverlord

---
+++ Assets/Scripts/Pulse.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pulse : MonoBehaviour 
+{
+	private float defaultScale;
+
+	void Start () 
+	{
+		defaultScale = transform.localScale.x;
+	}
+	
+	void Update()
+	{
+		if(Overlord.Instance.TO.Beat)
+		{
+			transform.localScale = new Vector3 (transform.localScale.x + 10f, transform.localScale.y + 10f, transform.localScale.z) ;
+		}
+		
+		if(transform.localScale.x > defaultScale)
+		{
+			transform.localScale = new Vector3 (transform.localScale.x - .5f, transform.localScale.y - .5f, transform.localScale.z) ;
+		}
+	}
+}
